5df6dd1e60905c3e5b9394f4ed26d755a5738b62
Alireza Bandegi <60815121+alirezabandegi@users.noreply.github.com>
2024-11-07 03:46:06 +0200

    Goal scripts

--- main.py
+++ main.py
@@ -14,14 +14,32 @@ def set_user_level():
         print("Invalid input. Please choose one of the following options: Beginner, Intermediate, or Advanced.")
 
       
+def set_user_goal():
+    """
+    Prompts the user to select their fitness goal.
+    Returns:
+        str: User's fitness goal ('muscle gain', 'endurance', 'strength building', 'weight loss').
+    """
+    valid_goals = {"muscle gain", "endurance", "strength building", "weight loss"}
+    prompt = "What is your goal? (Muscle Gain, Endurance, Strength Building, Weight Loss): "
+
+    while True:
+        user_input = input(prompt).strip().lower()
+        if user_input in valid_goals:
+            return user_input
+        print("Invalid input. Choose from Muscle Gain, Endurance, Strength Building, or Weight Loss.")
+
+
 def main():
     """
     Main function to gather user's fitness profile.
     """
     user_level = set_user_level()
+    user_goal = set_user_goal()
 
     print("\nUser Profile:")
     print(f"Level: {user_level.capitalize()}")
+    print(f"Goal: {user_goal.capitalize()}")
 
 
 if __name__ == "__main__":
